2127c6b58d0a673f5b08cd778c8aa29ab5918a2e
Charles Hall <chall8001@gmail.com>
2021-08-04 10:56:12 -0600

    added express backend

---
+++ server/index.js
@@ -0,0 +1,14 @@
+//importing express
+const express = require('express'); 
+//creating the global server variable(making our server)
+const app = express(); 
+//creating the listening port-- either where its deployed, or local host 5000
+const port = process.env.PORT || 5000; 
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
+
+// create a GET route
+app.get('/test', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED' }); 
+}); 
